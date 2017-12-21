6be70b558c457d6913606f1b64107f6e0e417724
Justin Bell <justinbell714@gmail.com>
2017-12-21 10:09:25 -0800

    Text adjustment

--- pin-lite-client/src/Components/SavedPins.js
+++ pin-lite-client/src/Components/SavedPins.js
@@ -9,7 +9,6 @@ class SavedPins extends Component {
     return (
       <div className="SavedPins">
       <p>Feature Coming Soon!</p>
-      <p>Did not quite get here :/ as I wanted to make my personal Thursday morning (Oct 12, 2017) deadline to submit the MVP</p>
       </div>
     );
   }
