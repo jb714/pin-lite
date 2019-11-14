adda052c39fc0d5719171ec7c4d42a470e57731e
Justin Bell <justinbell714@gmail.com>
2019-11-14 08:59:16 -0800

    Refactoring file structure

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const appRoutes = require('./pin-lite-backend/appRoutes');
 const path = require('path');
 const cors = require('cors');
 const port = process.env.PORT || 8080;
+const allPins = require('./Data/pins_formatted.json');
 
 app.listen(port, function(){
   console.log("Now listening on Port "  + port);
@@ -12,6 +12,15 @@ app.listen(port, function(){
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'pin-lite-client/build')));
+
+app.get("/", function(req,res){
+  res.send("Welcome Home");
+});
+
+app.get("/allPins", function(req,res){
+  res.json(allPins);
+  console.log("Pins have been sent");
+});
 
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, 'pin-lite-client/build/index.html'))
