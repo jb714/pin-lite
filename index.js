6f8c52dbfff920aae16270bb2bd2af605a82f698
Justin Bell <justinbell714@gmail.com>
2019-11-14 08:49:43 -0800

    Adding in proxy

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const app = express();
 const appRoutes = require('./pin-lite-backend/appRoutes');
 const path = require('path');
 const cors = require('cors');
-const proxy = require('http-proxy-middleware');
 const port = process.env.PORT || 8080;
-
-module.exports = function(app) {
-  app.use(proxy(['/allPins'], { target: 'http://localhost:8080'}))
-}
 
 app.listen(port, function(){
   console.log("Now listening on Port "  + port);
-  console.log("AppRoutes! "  + appRoutes);
 });
 
 app.use(cors());
